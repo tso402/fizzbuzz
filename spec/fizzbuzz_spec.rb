b12b1f8587ef4b97d0adcc11768045900d605d00
tso402 <james.ault@johnlewis.co.uk>
2019-01-29 16:44:37 +0000

    Failing test that 1 returns 1

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -24,6 +24,10 @@ describe 'fizzbuzz' do
   it 'returns "fizzbuzz" when passed 15' do
     expect(fizzbuzz(15)).to eq 'fizzbuzz'
   end
+  
+  it 'returns 1 when passed 1' do
+    expect(fizzbuzz(1)).to eq 1
+  end
 end
 
 
