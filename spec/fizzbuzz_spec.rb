770d3e61a78785c8c6387e8175d906fa36c32e87
Lima Vadakara <lima.vadakara@gmail.com>
2019-01-29 15:52:10 +0000

    write test 4

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -12,6 +12,10 @@ describe 'fizzbuzz' do
   it 'returns "buzz" when passed 5' do
     expect(fizzbuzz(5)).to eq 'buzz'
   end
+  
+  it 'returns "fizz" when passed 9' do
+    expect(fizzbuzz(9)).to eq 'fizz'
+  end
 end
 
 
