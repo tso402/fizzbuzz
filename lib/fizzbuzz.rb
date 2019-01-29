d31a8365ddb156dcc233c4b8edd1d807eebaee4f
tso402 <james.ault@johnlewis.co.uk>
2019-01-29 16:40:22 +0000

    Pass test for multiple of 3 and 5 is fuzzbuzz

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -2,7 +2,9 @@ def fizzbuzz(number)
     if !number.is_a? Integer 
        return 'Please enter an integer'
     end
-    if number % 3 == 0
+    if number % 3 == 0 and number % 5 == 0
+      'fizzbuzz'
+    elsif number % 3 == 0
       'fizz'
     elsif number % 5 == 0
       'buzz'
